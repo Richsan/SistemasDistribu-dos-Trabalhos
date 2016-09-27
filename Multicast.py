2a3e91477fb978a5f2bb63290028c6c334c63615
Henrique(Rich) <rchsan8@gmail.com>
2016-09-27 17:05:25 -0300

    numberOfProcess as argument

--- Multicast.py
+++ Multicast.py
@@ -1,6 +1,6 @@
 import socket
 import struct
-import sys
+from sys import argv
 import os
 import thread
 import time
@@ -21,23 +21,29 @@ class MulticastChat:
     numberAckList = {}
     pid_list = [pid]
     time = 0
-    numberOfProcess = 3
     server_address = ((hostIp,10000))
+    conversationList = []
     
-    def __init__(self):
+    def __init__(self, numberOfProcess):
+        self.numberOfProcess = numberOfProcess
+
         self.sock = self.initConnection()
         self.synchronize(self.numberOfProcess)
 
         self.pid_list.sort()
         self.pid = self.pid_list.index(self.pid)
-        self.time = self.pid + 1
+        
         
         thread.start_new_thread(self.recvListener,())
-        print "Ready - I'm the process no: "+str(self.pid)
+        print "Ready - "+self.presentationMsg()
         print "Send your message:"
         while True:
             msg = raw_input()
-            if msg[:3] == "STP":
+            if msg[:4] == "LIST":
+                self.printConversationLog()
+            elif msg[:3] == "PID":
+                print self.presentationMsg()     
+            elif msg[:3] == "STP":
                 pidStop = int(msg[4:].split("-s")[0])
                 seconds = int(msg[4:].split("-s")[1])
                 self.sendStopMsg(pidStop,seconds)
@@ -45,7 +51,13 @@ class MulticastChat:
                 self.sendMsg(msg)
         print "end"
 
-    
+    def presentationMsg(self):
+        return "I'm the process no: "+str(self.pid)
+
+    def printConversationLog(self):
+        for i in self.conversationList:
+                print i
+                    
     def initConnection(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -80,22 +92,27 @@ class MulticastChat:
         self.sock.sendto(msg,self.multicast_group)
 
     def sendMsg(self,msg):
+        self.time += 1
         msgTosend = "MSG ID:" + str(self.time) + "-" + str(self.pid) + "\r\nContent:" + msg
         #print "sended: "+msgTosend
         self.sock.sendto(msgTosend,self.multicast_group)
         
 
-    def recvMsg(self,msg, msgId, timeRecv):
-        self.queueMsg.put((msgId,msg))
+    def recvMsg(self,msg, msgId, timestamp,senderId):
+        msgGuard = str(senderId) + " - Time "+str(timestamp)+": " + msg.split(":")[1] 
+        self.queueMsg.put((msgId,msgGuard))
         
-        self.time = 1 + max(self.time,timeRecv)
+        self.time = 1 + max(self.time,timestamp)
         self.ackMsg(msgId)
 
     def ackMsg(self, msgId):
-        ack = "ACK "+str(msgId)
+        ack = "ACK "+str(msgId) + ":" + str(self.time)
+        self.time += 1
         self.sock.sendto(ack,self.multicast_group)
         
-    def recvAck(self,msgId):
+    def recvAck(self,msgId, timestamp):
+        self.time = 1 + max(self.time, timestamp)
+        
         if msgId in self.numberAckList:
             self.numberAckList[msgId] += 1
         else:
@@ -112,8 +129,10 @@ class MulticastChat:
             self.queueMsg.put((time,msg))
             return
 
+        self.conversationList.append(msg)
         print msg
-
+        self.time +=1
+        
         del self.numberAckList[msgId]
 
         while not (self.queueMsg.empty()):
@@ -122,8 +141,9 @@ class MulticastChat:
                 self.queueMsg.put((time, msg))
                 break
 
+            self.conversationList.append(msg)
             print msg
-
+            self.time += 1
             del self.numberAckList[time]
 
     def recvListener(self):
@@ -133,17 +153,19 @@ class MulticastChat:
             cmd = data[:3]
 
             if cmd == "ACK":
-                msgID = int(data[4:])
-                self.recvAck(msgID)
+                msgID = int(data.split(":")[0][4:])
+                timestamp = int(data.split(":")[1])
+                self.recvAck(msgID,timestamp)
                 self.deliverMsg(msgID)
                 
 
             elif cmd == "MSG":
                 msgId, contentMsg =  data.split("\r\n")
                 timeRecv = int(msgId[7:].split("-")[0])
+                senderId = int(msgId[7:].split("-")[1])
                 msgId = int("".join(msgId[7:].split("-")))
                 content = str(msgId) + ": " + contentMsg[8:]
-                self.recvMsg(content,msgId,timeRecv)
+                self.recvMsg(content,msgId,timeRecv,senderId)
                 
             elif cmd == "STP":
                 stopPid = int(data[4:].split("-s")[0])
@@ -153,5 +175,8 @@ class MulticastChat:
                 print "Stopped to listen for %d seconds"%seconds
                 time.sleep(seconds)
                 print "Woke up"
-        
-MulticastChat()
+
+if len(argv) <= 1:
+    raise Exception("You must provide the number of process as argument!")
+else:
+    MulticastChat(int(argv[1]))
