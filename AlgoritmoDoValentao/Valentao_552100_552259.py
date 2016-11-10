a170a454867ae7371d3ee0b1c85f12868b0a92a7
Henrique(Rich) <rchsan8@gmail.com>
2016-11-10 06:13:14 -0200

    atualizacao

--- AlgoritmoDoValentao/Valentao_552100_552259.py
+++ AlgoritmoDoValentao/Valentao_552100_552259.py
@@ -25,116 +25,109 @@ from sys import argv
 
 class Node:
 
-    __connIdx = 0
-    __threadLock = threading.Lock()
-    __connSocket = socket(AF_INET, SOCK_STREAM)
+    multicast_group = ('224.3.29.71',10000)
+    multicast_ip = '224.3.29.71'
+    hostIp = '0.0.0.0'
+    server_address = ((hostIp,10000))
     __bufferSize = 1026
-    __clientSocket = socket(AF_INET, SOCK_STREAM)
     __running = True
     __leader = 1
     __leaderFlag = False
     
-    def __init__(self, hostname,id,numNodes,idList):
+    def __init__(self, hostname,id):
         self.id = id
-        self.__port = 12000+id
-        self.__connSocketList = {}
-        self.__numNodes = numNodes
-        self.hostname = hostname
-        self.connectToOthers(idList)
-        t = Thread(target = self.connListener, args = (hostname,))
+        self.sock = socket(AF_INET, SOCK_DGRAM)
+
+        membership = inet_aton(self.multicast_ip) + inet_aton(self.hostIp)
+                
+        self.sock.setsockopt(IPPROTO_IP, IP_ADD_MEMBERSHIP, membership)
+        self.sock.setsockopt(SOL_SOCKET, SO_REUSEPORT, 1)
+
+        self.sock.bind(self.server_address)
+        t = Thread(target = self.msgRecvListener, args = ())
         t.start()
+        
         self.msgSender()
         
-    def connListener(self, hostname):
-        self.__connSocket
-        self.__connSocket.bind((hostname,self.__port))
-        self.__connSocket.listen(1)
-
-        while self.__numNodes > 0:
-            conn, _ = self.__connSocket.accept()
-            self.__connSocketList[self.__connIdx] = conn
-            t = Thread(target = self.msgRecvListener, args = (conn,self.__connIdx,))
-            self.__connIdx += 1
-            t.start()
-            self.__numNodes -= 1
             
             
     def msgSender(self):
         while self.__running:
             if self.__leader == self.id:
-                print("leader")
-            msg = raw_input(">")
+                msg = raw_input("leader>")
+            else:
+                msg = raw_input(">")
+                
             if msg == "CLOSE":
                 self.close()
                 break
             self.sendMulticastMsg(msg)
 
             
-    def msgRecvListener(self, sock, connId):
+    def msgRecvListener(self):
         while self.__running:
-            msg =  sock.recv(self.__bufferSize);
+            msg, _ =  self.sock.recvfrom(self.__bufferSize);
             if not self.__running:
                 break
-            self.recvMsg(msg,sock,connId)
-            if msg[:4] == "QUIT":
-                break;
+            self.recvMsg(msg)
+            
 
-    def recvMsg(self, msg,sock,connId):
-        print("Received: " + msg)
-        
+    def recvMsg(self, msg):
+                
         if msg[:4] == "QUIT":
             idSender = int(msg[5:])
-            del self.__connSocketList[connId]
-            sock.send("QUIT"+str(self.id))
-            
+            if idSender != self.id:
+                print("Received: " + msg)
+                
             if idSender == self.__leader:
                 t = Thread(target = self.startElection, args = ())
                 t.start()
-
-            
-                
+                        
         elif msg[:2] == "OK":
             idSender = int(msg[3:])
-            if self.__leaderFlag and idSender >= self.id:
+
+            if(idSender > self.id):
+                print("Received: " + msg)
+            
+            if self.__leaderFlag and idSender > self.id:
                 self.__leaderFlag = False
 
         elif msg[:8] == "ELECTION":
             idSender = int(msg[9:])
+
             if idSender < self.id:
-                self.sendOkMsg(sock)
+                print("Received: " + msg)
+                self.sendOkMsg()
                 if not self.__leaderFlag:
                     t = Thread(target = self.startElection,args = ())
                     t.start()
                 
         elif msg[:6] == "LEADER":
             idSender = int(msg[7:])
+
+            if idSender != self.id:
+                print("Received: " + msg)
+
+            
             if idSender != self.__leader:
                 self.__leader = idSender
-                print("Sending: "+msg)
-                self.sendMulticastMsg(msg)
-        
+
+        else:
+            print("Received: " + msg)
+                
     def sendMulticastMsg(self,msg):
-        for i in self.__connSocketList:
-            self.__connSocketList[i].send(msg)
+        self.sock.sendto(msg,self.multicast_group)
+    
 
-    def connectToOthers(self,idlist):
-        for i in idList:
-            connSock = socket(AF_INET, SOCK_STREAM)
-            connSock.connect((self.hostname,12000+i))
-            self.__connSocketList[self.__connIdx] = connSock
-            t = Thread(target = self.msgRecvListener, args = (connSock,self.__connIdx,))
-            self.__connIdx += 1
-            t.start()
-
-
-    def sendOkMsg(self,sock):
+    def sendOkMsg(self):
         print("Sending: "+"OK "+str(self.id))
-        sock.send("OK "+str(self.id))
+        self.sendMulticastMsg("OK "+str(self.id))
         
     def close(self):
-        self.sendMulticastMsg("QUIT "+str(self.id))
         self.__running = False
-        self.__connSocket.close()
+        self.sendMulticastMsg("QUIT "+str(self.id))
+        
+
 
     def startElection(self):
         self.__leaderFlag = True
@@ -158,9 +151,5 @@ class Node:
 if len(argv) <= 1:
     raise Exception("You must provide the id of processes as argument!")
 else:
-    idList = []
-
-    for i in range(3,len(argv)):
-        idList.append(int(argv[i]))
-        
-    Node('',int(argv[1]),int(argv[2]),idList)
+    
+    Node('',int(argv[1]))
